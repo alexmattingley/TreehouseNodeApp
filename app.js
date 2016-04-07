8c094c1dfc7c9f46e9a2b4826fc0311df1788c23
Alex Mattingley <alexmattingely@gmail.com>
2016-04-07 09:47:04 -0700

    modified app.js

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ var http = require('http');
 
 //create web server
 http.createServer(function (request, response) {
-	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.write("Header\n");
 	router.home(request,response);
 	router.user(request,response);
-	response.end('footer\n');
 }).listen(3000, '127.0.0.1');
 
 //require router file
